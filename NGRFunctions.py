7e6f7e022386dff6f754f396ac3e84c307f9839f
ColeWardReal <cole920ward@gmail.com>
2023-12-06 14:42:17 -0600

    Reorganized, added comments, added more classes, really cool logger data tracking features, colored output in console, new structure for data output. Need to work on color output for .log, working on error fail safes, and begin the actual project

--- NGRFunctions.py
+++ NGRFunctions.py
@@ -1,13 +1,24 @@
 import logging
+import copy
 
+# CONSTANTS SECTION
+# The background is set with 40 plus the number of the color, and the foreground with 30
 BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
 
-# The background is set with 40 plus the number of the color, and the foreground with 30
-
 # These are the sequences need to get colored ouput
+DEFAULT_IMPORTANCE = "Unknown" # not ready for implementation yet
 RESET_SEQ = "\033[0m"
 COLOR_SEQ = "\033[1;%dm"
 BOLD_SEQ = "\033[1m"
+
+COLORS = {
+    "WARNING": YELLOW,
+    "INFO": WHITE,
+    "DEBUG": BLUE,
+    "CRITICAL": CYAN,
+    "ERROR": RED
+}
+# END OF CONSTANTS SECTION
 
 
 def formatter_message(message, use_color=True):
@@ -18,57 +29,68 @@ def formatter_message(message, use_color=True):
     return message
 
 
-COLORS = {
-    'WARNING': YELLOW,
-    'INFO': WHITE,
-    'DEBUG': BLUE,
-    'CRITICAL': CYAN,
-    'ERROR': RED
-}
-
-
 class ColoredFormatter(logging.Formatter):
+
     def __init__(self, msg, use_color=True):
         logging.Formatter.__init__(self, msg)
         self.use_color = use_color
 
     def format(self, record):
-        levelname = record.levelname
+        # Create a copy of the record before changing it
+        record_copy = copy.copy(record)
+
+        levelname = record_copy.levelname
         if self.use_color and levelname in COLORS:
             levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
-            record.levelname = levelname_color
-        return logging.Formatter.format(self, record)
+            record_copy.levelname = levelname_color
+
+        # Call the parent class's format method with the copied record
+        return logging.Formatter.format(self, record_copy)
+
+# for better logging severity within the example log
+class ImportanceFormatter(logging.Formatter):
+    def format(self, record):
+        # Map the level number to an importance value
+        level_to_importance = {
+            logging.DEBUG: "Low",
+            logging.INFO: "Medium",
+            logging.WARNING: "High",
+            logging.ERROR: "Critical",
+            logging.CRITICAL: "Critical"
+        }
+        importance = level_to_importance.get(record.levelno, "Unknown")
+
+        # Add the importance value to the record
+        record.importance = importance
+
+        # Call the parent class's format method
+        return super().format(record)
 
 
-# Custom logger class with multiple destinations
+# logger class with multiple destinations, Console Stream (has colors, very cool) and File Stream (example.log)
 class ColoredLogger(logging.Logger):
-    FORMAT = "%(asctime)s - [%(levelname)-20s] | %(message)s ($BOLD%(filename)s$RESET:%(lineno)d)"
-
-    COLOR_FORMAT = formatter_message(FORMAT, True)
+    """A logger that supports colored and importance-level log messages."""
 
     def __init__(self, name):
-        logging.Logger.__init__(self, name, logging.DEBUG)
+        super().__init__(name, logging.DEBUG)
 
-        color_formatter = ColoredFormatter(self.COLOR_FORMAT)
+        # Define format strings
+        formatCL = "%(asctime)s - [%(levelname)-s] | %(message)s | ($BOLD%(filename)s$RESET:%(lineno)d)"
+        formatFH = "%(asctime)s - %(levelname)-s | IMPORTANCE: %(importance)s | %(message)s (%(filename)s: Line Number %(lineno)d)"
 
+        # create formatters
+        color_formatter = ColoredFormatter(formatter_message(formatCL, True))
+        file_formatter = ImportanceFormatter(formatFH)
+
+        # create console handler and set formatter
         console = logging.StreamHandler()
         console.setFormatter(color_formatter)
-
         self.addHandler(console)
 
-        # create file handler
-        # fix later, output is '1mFNAME.py'
+        # create file handler and set formatter
         fh = logging.FileHandler("example.log")
-        fh.setFormatter(color_formatter)
+        fh.setFormatter(file_formatter)
         self.addHandler(fh)
-
-        return
-
-
-# def format(self, record):
-#     log_fmt = self.FORMATS.get(record.levelno)
-#     formatter = logging.Formatter(log_fmt)
-#     return formatter.format(record)
 
 
 logging.setLoggerClass(ColoredLogger)
